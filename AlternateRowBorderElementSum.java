de6c8364544d9ea383d3fdad5c70f91864073d6f
venkatapraneethreddy <mvpreddy05@gmail.com>
2025-07-18 10:05:58 +0530

    coding practice

--- AlternateRowBorderElementSum.java
+++ AlternateRowBorderElementSum.java
@@ -1,26 +1,26 @@
 package LogicWhile;
 import java.util.Scanner;
 
-public class AlternateRowBorderElementSUm {
+public class AlternateRowBorderElementSum {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int a[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                a[i][j]=sc.nextInt();
-            }
-        }
-        
-        for(int i=n-1;i>=0;i-=2){
-            for(int j=0;j<m;j++){
-                if(i%2==0){
-                    
-                    System.out.print(a[i][j]+" ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int sum = 0;
+        int[][] a = new int[n][m];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                a[i][j] = sc.nextInt();
+
+        for (int i = 0; i < n; i += 2) {
+            for (int j = 0; j < m; j++) {
+                if (j == 0 || j == m - 1) {
+                    sum += a[i][j];
                 }
             }
-            System.out.println();
         }
+
+        System.out.println(sum);
     }
 }
